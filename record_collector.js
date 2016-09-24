98fcd26b10d17d54772b05b34b86d0609692df13
Phil Crooks <pacman@crooks.me.uk>
2016-09-24 19:41:34 +0100

    record collector and tests added

---
+++ record_collector.js
@@ -0,0 +1,33 @@
+RecordCollector.prototype.buy = function(record) {
+  if (this._cash > record.price) {
+    this._cash -= record.price;
+    this._collection.push(record.copy());
+    return true;
+  }
+  return false;
+}
+
+RecordCollector.prototype.sell = function(record) {
+  for (var c = 0, n = this._collection.length; c < n; c++) {
+    if (this._collection[c] === record) {
+      this._cash += record.price;
+      this._collection.splice(c, 1);
+      return record;
+    }
+  }
+  return null;
+}
+
+function RecordCollector(name) {
+  this._name = name;
+  this._collection = [];
+  this._cash = 0;
+  Object.defineProperty(this, "name", { get: function () { return this._name} });
+  Object.defineProperty(this, "collectionSize", { get: function () { return this._collection.length} });
+  Object.defineProperty(this, "cash", {
+    set: function (value) { this._cash = value},
+    get: function () { return this._cash }
+  });
+}
+
+module.exports = RecordCollector;
--- specs/record_collector_spec.js
+++ specs/record_collector_spec.js
@@ -1,1 +1,53 @@
-// Create a RecordCollector (or customer) constructor who can buy and sell records.
+var assert = require( 'assert' );
+var Record = require( '../record' );
+var RecordCollector = require( '../record_collector' );
+
+describe ( 'Record Collector', function(){
+  var recordCollector;
+
+  before(function() {
+    // Create a constructor to create Record objects with artist, title, price
+
+    // Create a RecordCollector (or customer) constructor who can buy and sell records.
+    recordCollector = new RecordCollector("Steve");
+  })
+
+  it ( 'has a name', function() {
+    assert.strictEqual(recordCollector.name, "Steve");
+  })
+
+  it ( 'has no records', function() {
+    assert.strictEqual(recordCollector.collectionSize, 0);
+  })
+
+  it ( 'has no cash', function(){
+    assert.strictEqual(recordCollector.cash, 0);
+  })
+
+  it ( "can't buy a record without any money", function() {
+    var record = new Record("T.Rex", "Bolan Boogie", 2.10);
+    assert.strictEqual(recordCollector.buy(record), false);
+  })
+
+  it ( 'has £5.00', function() {
+    recordCollector.cash = 5.00;
+    assert.strictEqual(recordCollector.cash, 5.00);
+  })
+
+  it ( 'can buy a record', function() {
+    var record = new Record("T.Rex", "Bolan Boogie", 2.10);
+    assert.strictEqual(recordCollector.buy(record), true);
+    assert.strictEqual(recordCollector.collectionSize, 1);
+    assert.strictEqual(recordCollector.cash, 2.90);
+  })
+
+  it ( 'can sell a record', function() {
+    var record = recordCollector._collection[0];
+    assert.strictEqual(recordCollector.sell(record), record);
+    assert.strictEqual(recordCollector.collectionSize, 0);
+    assert.strictEqual(recordCollector.cash, 5.00);
+  })
+})
+
+
+
